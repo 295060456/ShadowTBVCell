18005fca1420ca3fc47841ada916839ed7126902
Jobs <lg295060456@gmail.com>
2021-04-22 10:40:30 +0700

    no message

--- ShadowTBVCell/🌹Macro/第三方头文件管理/手动导入的第三方/Manual_Add_ThirdParty.h
+++ ShadowTBVCell/🌹Macro/第三方头文件管理/手动导入的第三方/Manual_Add_ThirdParty.h
@@ -9,57 +9,7 @@
 #ifndef Manual_Add_ThirdParty_h
 #define Manual_Add_ThirdParty_h
 
-/// BaseCustomizeUIKit
-#pragma mark —— Log
-#import "DebugLogDescription.h"
-#pragma mark —— UIButton
-#import "UIButton+ImageTitleSpacing.h"
-#pragma mark —— NSString
-#import "NSString+Extras.h"
-#pragma mark —— UIView
-#import "LoadingImage.h"
-// BaseView
-#import "BaseView.h"
-// UIView+Category
-#import "UIView+Extras.h"
-#import "UIView+EmptyData.h"
-#import "UIView+MJRefresh.h"
-#import "UIView+Measure.h"
-#import "UIView+ViewController.h"
-#pragma mark —— NSObject
-#import "NSObject+Extras.h"
-#import "NSObject+Shake.h"
-#import "NSObject+DefConfig.h"
-#import "NSObject+OpenURL.h"
-#import "NSObject+CallBackInfoByBlock.h"
-#pragma mark —— UIViewController
-#import "UIViewController+BaseVC.h"
-#import "UIViewController+BackBtn.h"
-#import "UIViewController+BRPickerView.h"
-#import "UIViewController+BWShareView.h"
-#import "UIViewController+GifImageView.h"
-#import "UIViewController+JPImageresizerView.h"
-#import "UIViewController+JXCategoryListContentViewDelegate.h"
-#import "UIViewController+JXPagerViewListViewDelegate.h"
-#import "UIViewController+MJRefresh.h"
-#import "UIViewController+Shake.h"
-#import "UIViewController+BaseNavigationBar.h"
-#import "UIViewController+EmptyData.h"
-#pragma mark —— UINavigationController
-#import "BaseNavigationVC.h"
-#pragma mark —— UINavigationBar
-#import "BaseNavigationBar.h"
-#pragma mark —— UITableViewCell
-#import "UITableViewCell+Margin.h"
-#pragma mark —— UIImageView
-/// BaseImageView
-#import "BaseImageView.h"
-/// UIImageView+Category
-#import "UIImageView+GIF.h"//UIImageView支持GIF动画 https://github.com/pupboss/UIImageView-GIF
-#pragma mark —— UITextView
-/// UITextView+Category
-#import "UITextView+IndicateWordLimit.h"
-#import "UITextView+Extend.h"
+#import "JobsBaseCustomizeUIKitCoreHeader.h"
 
 #pragma mark —— Manual_Add_ThirdParty
 #import "UserDefaultManager.h"
